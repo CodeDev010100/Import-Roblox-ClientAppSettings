515d697f54f81a7f3edf395101f4f520e8562ac4
CodeDev010100 <97323454+CodeDev010100@users.noreply.github.com>
2023-06-12 00:43:07 -0300

    Update RobloxOtipmizer.py

--- Python Projects/Roblox/RobloxOtipmizer.py
+++ Python Projects/Roblox/RobloxOtipmizer.py
@@ -1,37 +1,41 @@
 from sys import exit
 from time import sleep
-from os import getenv, linesep, system
+from os import linesep
 from os.path import exists
 from pathlib import Path
 from urllib.request import urlopen, urlretrieve
 from json import loads
-import shutil
 
 CLIENT_VERSION_JSON = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag").read()
-CLIENT_VERSION = loads(CLIENT_VERSION_JSON)["clientVersionUpload"]
+STUDIO_VERSION_JSON = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsStudio/channel/zflag").read()
+
+CLIENT_UPLOADED_VERSION = loads(CLIENT_VERSION_JSON)["clientVersionUpload"]
+STUDIO_UPLOADED_VERSION = loads(STUDIO_VERSION_JSON)["clientVersionUpload"]
 
 ROBLOX_VERSIONS_FOLDER = Path.home() / 'AppData' / 'Local' / 'Roblox' / 'Versions'
-ROBLOX_VERSION_FOLDER = None
 
-print("Client version hash: "+CLIENT_VERSION)
+CLIENTAPPSETTINGS_JSON = urlopen("https://raw.githubusercontent.com/L8X/Roblox-Client-Optimizer/main/ClientAppSettings.json").read().decode().rstrip(linesep)
 
-CLIENTAPPSETTINGS_JSON = urlopen("https://raw.githubusercontent.com/L8X/Roblox-Client-Optimizer/main/assets/ClientAppSettings.json").read().decode().rstrip(linesep)
+def Import_ClientAppSettings(UPLOADED_VERSION):
+    print("[Information] Client version hash: "+ UPLOADED_VERSION)
+    VERSION_FOLDER = Path(ROBLOX_VERSIONS_FOLDER) / UPLOADED_VERSION
 
-if exists(ROBLOX_VERSIONS_FOLDER):
-    ROBLOX_VERSION_FOLDER = Path(ROBLOX_VERSIONS_FOLDER) / CLIENT_VERSION
-    print("Found \""+ROBLOX_VERSION_FOLDER.name+"\" folder!")
-else:
-    print("[Error]: Could not find the \""+ROBLOX_VERSION_FOLDER.name+"\" folder!")
-    exit(1)
-
-caminho_pasta = Path(ROBLOX_VERSION_FOLDER) / 'ClientSettings'
-caminho_pasta.mkdir()
-json = Path(ROBLOX_VERSION_FOLDER) / 'ClientSettings' / 'ClientAppSettings.json'
-json.touch()
-json.write_text(CLIENTAPPSETTINGS_JSON)
-
-for _ in range(3,0,-1):
-    system("title "+"Closing in "+_.__str__())
-    print('.')
-    sleep(1)
-exit(0)
+    if exists(VERSION_FOLDER):
+        print("[Sucess] Found \""+VERSION_FOLDER.name+"\" folder!")
+        caminho_pasta = Path(VERSION_FOLDER) / 'ClientSettings'
+        if exists(caminho_pasta):
+            print("[Error]: the \""+caminho_pasta.name+"\" folder has already been inserted")
+        else:
+            caminho_pasta.mkdir()
+            print("[Sucess] \""+caminho_pasta.name+"\" folder was placed in the folder \""+VERSION_FOLDER.name+"\"")
+            json = Path(VERSION_FOLDER) / 'ClientSettings' / 'ClientAppSettings.json'
+            json.touch()
+            json.write_text(CLIENTAPPSETTINGS_JSON)
+            print("[Sucess] ClientAppSettings imported")
+            sleep(2)
+            exit(0)
+    else:
+        print("[Error]: Could not find the \""+UPLOADED_VERSION+"\" folder!")
+        sleep(2)
+        exit(0)
+Import_ClientAppSettings(CLIENT_UPLOADED_VERSION) # Put in the parameter the version you want, you can put the client version or the studio version
